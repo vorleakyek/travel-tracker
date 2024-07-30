f7d35efad7d8c3eeed30512bed2014d9c09395c7
Vorleak Yek <vorleakyek@gmail.com>
2024-07-30 05:46:01 +0000

    setup the svg map first version

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,15 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+create table users(
+  id serial primary key,
+  name varchar(15) unique not null,
+  color varchar(15)
+);
+
+create table visited_countries(
+  id serial primary key,
+  country_code char(2) not null,
+  user_id integer references users(id)
+);
